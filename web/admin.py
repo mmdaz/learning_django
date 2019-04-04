74406d8a3af1a3b98ce65567ef052d340e5e809e
Muhammad <ma313@aut.ac.ir>
2019-04-04 19:51:56 +0430

    start again

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Expense , Income
+from .models import Expense , Income , Token
 from django.contrib import admin
 
 # Register your models here
 admin.site.register(Expense)
 admin.site.register(Income)
+admin.site.register(Token)
--- web/views.py
+++ web/views.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def submit_expense (request) :
+	""" submit an expense """
+	return 'I am here'
