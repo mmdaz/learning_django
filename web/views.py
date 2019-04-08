08174e1ae7960ba1b36958a1b355670626e38d50
Muhammad <ma313@aut.ac.ir>
2019-04-08 12:00:17 +0430

    add simple view

--- web/views.py
+++ web/views.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import json
+from json import JSONEncoder
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 
 def submit_expense(request):
-    return 'I am here'
+    print(request.POST)
+
+    return JsonResponse({
+        "status": "ok"
+    }, encoder=JSONEncoder)
