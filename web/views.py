ceefb94d587613ffd1d01536ebca32df666bdb83
Muhammad <ma313@aut.ac.ir>
2019-04-10 12:37:31 +0430

    add a expense with postman

--- web/views.py
+++ web/views.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import json
+
+import datetime
 from json import JSONEncoder
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from web.models import Token, Income, User, Expense
 
 
-# Create your views here.
-
+@csrf_exempt
 def submit_expense(request):
+    # TODO : validate data
     print(request.POST)
-
+    token = request.POST['token']
+    user = User.objects.filter(token__token=token).get()
+    Expense.objects.create(text=request.POST['text'], date=datetime.datetime.now(), amount=request.POST['amount'],
+                           user=user)
     return JsonResponse({
         "status": "ok"
     }, encoder=JSONEncoder)
